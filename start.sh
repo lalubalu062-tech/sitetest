4dca9586420fbf3ef0543964ee617593453363b9
Lalu Kumar <lalubalu062@gmail.com>
2026-06-19 22:00:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Render ko shant rakhne ke liye dummy server
+# 1. Render ko timeout se bachane ke liye dummy server
 echo "Starting Dummy Server on Port 8080..."
 python3 -m http.server 8080 &
 
-# Otohits Viewer start karna aur direct Key pass karna
+# 2. Otohits start karna (Ab ye direct Render Environment se key utha lega)
 echo "Starting Otohits Viewer..."
-# Render Dashboard se APPLICATION_KEY utha kar direct command me attach karega
-OtohitsApp -key=$APPLICATION_KEY || ./otohits-app -key=$APPLICATION_KEY
+./otohits-app
