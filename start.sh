6cbed3a00932c05eb9f3e09deecf2142c669cee0
Lalu Kumar <lalubalu062@gmail.com>
2026-06-19 21:50:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# Render ko shant rakhne ke liye dummy server
 echo "Starting Dummy Server on Port 8080..."
 python3 -m http.server 8080 &
 
+# Otohits Viewer start karna aur direct Key pass karna
 echo "Starting Otohits Viewer..."
-cd /app
-./otohits-app
+# Render Dashboard se APPLICATION_KEY utha kar direct command me attach karega
+OtohitsApp -key=$APPLICATION_KEY || ./otohits-app -key=$APPLICATION_KEY
